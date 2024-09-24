45b0bc669a3b5f4f552ac5995f58a33b668d29bf
Anthony3245 <anthony.yu26@ycdsbk12.ca>
2024-09-24 11:02:12 -0400

    Add files via upload

---
+++ age_in_five_years.py
@@ -0,0 +1,6 @@
+name = input("Hello.  What is your name? ")
+age = input ("Hi "+ name +  " How old are you? ")
+age_in_5_years = int(age) + 5
+age_5_years_ago = int(age) - 5
+print(f'your age in 5 years will be {age_in_5_years}')
+print(f'your age in 5 years ago was {age_5_years_ago}')
