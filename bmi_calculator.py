d2519eee8a17df592de256351120fee3ac35da1f
Anthony3245 <anthony.yu26@ycdsbk12.ca>
2024-09-24 10:44:21 -0400

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,3 +1,6 @@
+# Height in m: 1.75
+# Weight in kg: 73
+# The BMI is 23.83673
 height_m= 1.75
 weight_kg= 73
 BMI_calculator = weight_kg/height_m ** 2
@@ -12,15 +15,16 @@ height_m = height_inches * 0.0254
 weight_kg = weight_pounds * 0.453592
 BMI_calculator = weight_kg/height_m ** 2
 print(f"your BMI is {round(BMI_calculator,2)} ")
+
 # Height (feet only): 5
 # Height (inches): 9
 # Weight in pounds: 160
 # The BMI is 23.625289
 
 height_feet = 5
-height_inches = 9
+height_inches = 5
 total_height_inches = height_feet*12 + height_inches
-weight_pounds = 160
+weight_pounds = 130 
 height_m = total_height_inches* 0.0254
 weight_kg = weight_pounds * 0.453592
 BMI_calculator = weight_kg/height_m ** 2
