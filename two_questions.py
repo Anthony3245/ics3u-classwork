399676c319cc62bc37fe078a62a688ff10bed5eb
Anthony3245 <anthony.yu26@ycdsbk12.ca>
2024-10-01 11:10:50 -0400

    Add files via upload

---
+++ two_questions.py
@@ -0,0 +1,18 @@
+question_1 = input("animal, vegetable, or mineral?")
+question_2 = input("is it bigger than a breadbox?")
+if question_1 == "animal":
+    if question_2 == "yes":
+        print("ok you're thinking about a squirel")
+    elif question_2 == "no":
+        print(" ok you're thinking about a moose")
+elif question_1 == "vegetable":
+    if question_2 == "yes":
+        print("ok you're thinking about a carrot")
+    elif question_2 == "no":
+        print(" ok you're thinking about a watermelon")
+if question_1 == "mineral":
+    if question_2 == "yes":
+        print("ok you're thinking about a paper clip")
+    elif question_2 == "no":
+        print(" ok you're thinking about a Camero")
+
